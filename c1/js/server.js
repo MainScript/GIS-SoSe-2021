a6093d3257b799cadf732a22b0f4a739d5fd3826
Louciandra <37979646+MainScript@users.noreply.github.com>
2021-05-28 22:30:52 +0200

    update server

--- c1/js/server.js
+++ c1/js/server.js
@@ -19,7 +19,8 @@ var P_3_1Server;
         console.log("I hear voices!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        _response.write(_request.url);
+        console.log(_request.url);
         _response.end();
     }
 })(P_3_1Server = exports.P_3_1Server || (exports.P_3_1Server = {}));
