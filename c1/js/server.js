3a653b710c605f79663ef0f46c653fe277664881
Louciandra <37979646+MainScript@users.noreply.github.com>
2021-05-28 22:20:23 +0200

    added 3.1

---
+++ c1/js/server.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.P_3_1Server = void 0;
+const Http = require("http");
+var P_3_1Server;
+(function (P_3_1Server) {
+    console.log("Starting server");
+    let port = Number(process.env.PORT);
+    if (!port)
+        port = 8100;
+    let server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
+    function handleListen() {
+        console.log("Listening");
+    }
+    function handleRequest(_request, _response) {
+        console.log("I hear voices!");
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        _response.write(_request.url);
+        _response.end();
+    }
+})(P_3_1Server = exports.P_3_1Server || (exports.P_3_1Server = {}));
+//# sourceMappingURL=server.js.map
---
+++ c1/js/server.ts
@@ -0,0 +1,26 @@
+import * as Http from "http";
+
+export namespace P_3_1Server {
+    console.log("Starting server");
+    let port: number = Number(process.env.PORT);
+    if (!port)
+        port = 8100;
+
+    let server: Http.Server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
+
+    function handleListen(): void {
+        console.log("Listening");
+    }
+
+
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        console.log("I hear voices!");
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        _response.write(_request.url);
+        _response.end();
+    }
+}
