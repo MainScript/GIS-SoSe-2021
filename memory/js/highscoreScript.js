922d8cbe9c2c4037a8d0ad95057e6bfc6278f90d
MainScript03 <37979646+MainScript@users.noreply.github.com>
2021-07-09 23:05:54 +0200

    update memory

--- memory/js/highscoreScript.js
+++ memory/js/highscoreScript.js
@@ -8,12 +8,18 @@ var memoryHighscore;
         let outRes = await fetch(url);
         let out = await outRes.text();
         let scoreArr = JSON.parse(out);
-        for (let score of scoreArr) {
+        for (let i = 0; i < scoreArr.length; i++) {
             let scoreDiv = document.createElement("div");
             let nameP = document.createElement("p");
             let scoreP = document.createElement("p");
-            nameP.innerText = "Name: " + score.name;
-            scoreP.innerText = "Score: " + score.score;
+            nameP.innerText = "Name: " + scoreArr[i].name;
+            scoreP.innerText = "Score: " + scoreArr[i].score;
+            if (i % 2 == 0) {
+                scoreDiv.classList.add("scoreDivHell");
+            }
+            else {
+                scoreDiv.classList.add("scoreDivDunkel");
+            }
             scoreDiv.appendChild(nameP);
             scoreDiv.appendChild(scoreP);
             highscoresDiv.appendChild(scoreDiv);
--- memory/js/highscoreScript.ts
+++ memory/js/highscoreScript.ts
@@ -8,12 +8,17 @@ namespace memoryHighscore {
         let outRes: Response = await fetch(url);
         let out: string = await outRes.text();
         let scoreArr: Score[] = JSON.parse(out);
-        for (let score of scoreArr) {
+        for (let i: number = 0; i < scoreArr.length; i++) {
             let scoreDiv: HTMLDivElement = document.createElement("div");
             let nameP: HTMLParagraphElement = document.createElement("p");
             let scoreP: HTMLParagraphElement = document.createElement("p");
-            nameP.innerText = "Name: " + score.name;
-            scoreP.innerText = "Score: " + score.score;
+            nameP.innerText = "Name: " + scoreArr[i].name;
+            scoreP.innerText = "Score: " + scoreArr[i].score;
+            if (i % 2 == 0) {
+                scoreDiv.classList.add("scoreDivHell");
+            } else {
+                scoreDiv.classList.add("scoreDivDunkel");
+            }
             scoreDiv.appendChild(nameP);
             scoreDiv.appendChild(scoreP);
             highscoresDiv.appendChild(scoreDiv);
